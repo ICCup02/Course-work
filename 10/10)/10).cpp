4a681b64bd90d55b95da111d08b075cdd2b13183
ICCup02 <56939890+ICCup02@users.noreply.github.com>
2020-01-16 00:09:08 +0300

    Create 10).cpp

---
+++ 10/10)/10).cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream >
+using namespace std;
+int main() {
+	double a, b, x = 1;
+	cin >> a >> b;
+	if (b >= 1) {
+		for (int i = 0; i < b; i++) {
+			x *= a;
+		}
+	}
+	if (b <= 1) {
+		for (int i = 0; i > b; i--) {
+			x = (x * a);
+		}
+		if (x == 0) { cout << "sdsd";  return 0; }
+		x = 1 / x;
+	}
+	cout << x;
+	return 0;
+}
